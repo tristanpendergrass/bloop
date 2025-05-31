7dd87509b929e33514cafb11e31408a89f4cbec9
Tristan Pendergrass <tristanp@dropbox.com>
2025-05-31 10:27:38 -0700

    Add game

--- src/Main.elm
+++ src/Main.elm
@@ -5,6 +5,7 @@ import Browser.Events
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Random
 
 
 main : Program () Model Msg
@@ -17,12 +18,53 @@ main =
 
 
 type alias Model =
-    {}
+    { game : Maybe Game
+    , prevGames : List GameOutcome
+    }
+
+
+type alias Game =
+    { oxygen : Int
+    , treasure : List Treasure
+    , tiles : List Tile
+    , playerLocation : PlayerLocation
+    , playerDirection : Direction
+    , prevRoll : Maybe Int
+    }
+
+
+type PlayerLocation
+    = Submarine
+    | Water Int
+
+
+type Treasure
+    = Treasure Int
+
+
+type Tile
+    = TreasureTile Treasure
+    | EmptyTile
+
+
+type Direction
+    = Up
+    | Down
+
+
+type GameOutcome
+    = Drowned (List Treasure)
+    | Survived { treasure : List Treasure }
+
+
+type MoveResult
+    = GameContinues Game
+    | GameOver GameOutcome
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( {}, Cmd.none )
+    ( { game = Nothing, prevGames = [] }, Cmd.none )
 
 
 
@@ -31,7 +73,11 @@ init _ =
 
 type Msg
     = NoOp
-    | HandleAnimationFrameDelta Float
+    | HandleStartGamePress
+    | HandleStartGameResult Game
+    | HandleTurnAroundPress
+    | HandleRollPress
+    | HandleRollResult Int
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -40,8 +86,116 @@ update msg model =
         NoOp ->
             ( model, Cmd.none )
 
-        HandleAnimationFrameDelta _ ->
-            ( model, Cmd.none )
+        HandleStartGamePress ->
+            ( model, Random.generate HandleStartGameResult gameGenerator )
+
+        HandleStartGameResult gameResult ->
+            ( { model | game = Just gameResult }, Cmd.none )
+
+        HandleTurnAroundPress ->
+            case model.game of
+                Nothing ->
+                    ( model, Cmd.none )
+
+                Just game ->
+                    ( { model | game = Just { game | playerDirection = Up } }, Cmd.none )
+
+        HandleRollPress ->
+            ( model, Random.generate HandleRollResult (Random.int 2 6) )
+
+        HandleRollResult roll ->
+            case model.game of
+                Nothing ->
+                    ( model, Cmd.none )
+
+                Just game ->
+                    let
+                        adjustedRoll =
+                            roll
+                                |> adjustForWeight game
+                                |> adjustForDirection game
+
+                        newLocation =
+                            movePlayer game adjustedRoll game.playerLocation
+
+                        newOxygen =
+                            game.oxygen - List.length game.treasure
+                    in
+                    if newLocation == Submarine && game.playerDirection == Up then
+                        ( { model | game = Nothing, prevGames = Survived { treasure = game.treasure } :: model.prevGames }, Cmd.none )
+
+                    else if newOxygen <= 0 && newLocation /= Submarine then
+                        ( { model | game = Nothing, prevGames = Drowned game.treasure :: model.prevGames }, Cmd.none )
+
+                    else
+                        ( { model | game = Just { game | prevRoll = Just roll, playerLocation = newLocation, oxygen = newOxygen } }, Cmd.none )
+
+
+adjustForDirection : Game -> Int -> Int
+adjustForDirection game roll =
+    if game.playerDirection == Down then
+        roll
+
+    else
+        -1 * roll
+
+
+adjustForWeight : Game -> Int -> Int
+adjustForWeight game roll =
+    roll - List.length game.treasure
+
+
+movePlayer : Game -> Int -> PlayerLocation -> PlayerLocation
+movePlayer game amount location =
+    case location of
+        Submarine ->
+            Water amount
+
+        Water num ->
+            if num + amount < 0 then
+                Submarine
+
+            else if num + amount > List.length game.tiles - 1 then
+                Water (List.length game.tiles - 1)
+
+            else
+                Water (num + amount)
+
+
+gameGenerator : Random.Generator Game
+gameGenerator =
+    Random.map createGame tilesGenerator
+
+
+tilesGenerator : Random.Generator (List Tile)
+tilesGenerator =
+    let
+        smallTreasure =
+            Random.list 5 (tileGenerator 0 3)
+
+        mediumTreasure =
+            Random.list 5 (tileGenerator 4 6)
+
+        largeTreasure =
+            Random.list 5 (tileGenerator 7 9)
+    in
+    Random.map3 (\a b c -> a ++ b ++ c) smallTreasure mediumTreasure largeTreasure
+
+
+createGame : List Tile -> Game
+createGame tiles =
+    { oxygen = 20
+    , treasure = []
+    , tiles = tiles
+    , playerLocation = Submarine
+    , playerDirection = Down
+    , prevRoll = Nothing
+    }
+
+
+tileGenerator : Int -> Int -> Random.Generator Tile
+tileGenerator min max =
+    Random.map (Treasure >> TreasureTile) (Random.int min max)
 
 
 
@@ -51,8 +205,7 @@ update msg model =
 subscriptions : Model -> Sub Msg
 subscriptions _ =
     Sub.batch
-        [ Browser.Events.onAnimationFrameDelta HandleAnimationFrameDelta
-        ]
+        []
 
 
 
@@ -65,5 +218,56 @@ proseClass =
 
 
 view : Model -> Html Msg
-view _ =
-    div [ class "p-16", proseClass ] [ text "Why hello there" ]
+view model =
+    case model.game of
+        Nothing ->
+            div [] [ button [ class "btn", onClick HandleStartGamePress ] [ text "Start Game" ] ]
+
+        Just game ->
+            div []
+                [ div [] [ text "Oxygen: ", strong [] [ text (String.fromInt game.oxygen) ] ]
+                , div [] [ text "Weight: ", strong [] [ text (String.fromInt (List.length game.treasure)) ] ]
+                , h2 [] [ text "Treasure:" ]
+                , ul [] (List.map (\treasure -> li [] [ renderTreasure treasure ]) game.treasure)
+                , div [ class "divider" ] []
+                , div [ class "flex items-center gap-4" ]
+                    [ button [ class "btn btn-sm btn-primary", onClick HandleRollPress ] [ text "Roll" ]
+                    , button [ class "btn btn-sm btn-secondary", onClick HandleTurnAroundPress, disabled (game.playerDirection == Up) ] [ text "Go back up" ]
+                    , case game.prevRoll of
+                        Nothing ->
+                            text ""
+
+                        Just roll ->
+                            text ("Rolled: " ++ String.fromInt roll)
+                    ]
+                , ul [] (List.indexedMap (\index tile -> li [] [ renderTile game index tile ]) game.tiles)
+                ]
+
+
+renderTile : Game -> Int -> Tile -> Html Msg
+renderTile game index tile =
+    let
+        isPlayerLocation =
+            game.playerLocation == Water index
+
+        playerLocationEl =
+            if isPlayerLocation && game.playerDirection == Down then
+                span [ class "text-blue-500" ] [ text "👤" ]
+
+            else if isPlayerLocation && game.playerDirection == Up then
+                span [ class "text-blue-500" ] [ text "👤👆" ]
+
+            else
+                span [] []
+    in
+    case tile of
+        TreasureTile _ ->
+            span [] [ text "Treasure (??)", playerLocationEl ]
+
+        EmptyTile ->
+            span [] [ text "Empty", playerLocationEl ]
+
+
+renderTreasure : Treasure -> Html Msg
+renderTreasure (Treasure value) =
+    span [] [ text "Treasure (", strong [] [ text (String.fromInt value) ], text ")" ]
